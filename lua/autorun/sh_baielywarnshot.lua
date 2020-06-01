dde8bdb43bd5f991a60b0af5cc5f95979845f9fa
Baiely <54541772+Baiely@users.noreply.github.com>
2020-06-01 17:17:52 +0200

    Add files via upload

---
+++ lua/autorun/sh_baielywarnshot.lua
@@ -0,0 +1,16 @@
+BaielywarnshotConfig = {}
+
+BaielywarnshotConfig.Time = 7 -- Time until the warning message disappears
+
+BaielywarnshotConfig.Key = KEY_G
+
+BaielywarnshotConfig.Lang = {
+    victimtxt = "You were fired a warning shot!",
+    attackertxt = "The person noticed your warning shot!",
+}
+
+BaielywarnshotConfig.Colors = {
+    Primary = Color(45, 45, 45), --Default: Color(45,45,45)
+    Accent = Color(220, 57, 57), --Default: color(22,57,57)
+    Icon = Color(45, 45, 45) --Default: Color(255,255,255) (Only change when neccesary!!!!)
+}
